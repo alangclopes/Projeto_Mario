017c5150653da8bc0a5bb08b7fff5fa62fbf8674
alangclopes <145778088+alangclopes@users.noreply.github.com>
2024-06-09 11:03:12 -0300

    A

--- server.js
+++ server.js
@@ -5,24 +5,20 @@ const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-  session({
-    secret: "secredo",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
 
 app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 
-const candidatoRoutes = require("./routes/candidatoRoutes");
-const eleitorRoutes = require("./routes/eleitorRoutes");
-const votacaoRoutes = require("./routes/votacaoRoutes");
+const indexRouter = require("./routes/index");
+const candidatoRouter = require("./routes/candidatoRoutes");
+const eleitorRouter = require("./routes/eleitorRoutes");
+const votacaoRouter = require("./routes/votacaoRoutes");
 
-app.use("/candidatos", candidatoRoutes);
-app.use("/eleitores", eleitorRoutes);
-app.use("/votacao", votacaoRoutes);
+app.use("/", indexRouter);
+app.use("/candidatos", candidatoRouter);
+app.use("/eleitores", eleitorRouter);
+app.use("/votacao", votacaoRouter);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -30,5 +26,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
