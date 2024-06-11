fc93dfc056fae52c20545d0858b0d22f9fa720e4
LeonanAraudo <146787577+LeonanAraudo@users.noreply.github.com>
2024-06-10 21:40:21 -0300

    Add files via upload

--- views/CadastrarChapa/conect.js
+++ views/CadastrarChapa/conect.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/BuscaEleicao')
+    fetch('')
         .then(response => response.json())
         .then(data => {
             const select = document.getElementById('eleicao');
             data.forEach(eleicao => {
                 const option = document.createElement('option');
                 option.value = eleicao.id;
-                option.textContent = eleicao.nome + eleicao.local;
+                option.textContent = eleicao.nome;
                 select.appendChild(option);
             });
         })
