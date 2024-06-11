6d291333b0098784a5fbe66cd3d32b0188fa9216
alangclopes <145778088+alangclopes@users.noreply.github.com>
2024-06-11 13:24:44 -0300

    .

--- controllers/eleitorController.js
+++ controllers/eleitorController.js
@@ -1,82 +1,45 @@
-const pool = require("../config/db");
+const Eleitor = require("../models/eleitor");
+
+exports.create = async (req, res) => {
+  try {
+    await Eleitor.create(req.body);
+    res.redirect("/eleitores");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 
 exports.findAll = async (req, res) => {
   try {
-    const [rows] = await pool.query("SELECT * FROM eleitores");
-    res.json(rows);
+    const eleitores = await Eleitor.findAll();
+    res.render("eleitor/index", { eleitores });
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
 exports.findById = async (req, res) => {
-  const id = req.params.id;
   try {
-    const [rows] = await pool.query("SELECT * FROM eleitores WHERE id = ?", [
-      id,
-    ]);
-    if (rows.length > 0) {
-      res.json(rows[0]);
-    } else {
-      res.status(404).send("Eleitor not found");
-    }
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-};
-
-exports.create = async (req, res) => {
-  const { nome, cpf, predio, andar, numero_apt, senha } = req.body;
-
-  // Verificar se todos os campos necessários estão presentes
-  if (!nome || !cpf || !predio || !andar || !numero_apt || !senha) {
-    return res.status(400).send("All fields are required");
-  }
-
-  try {
-    const [result] = await pool.query(
-      "INSERT INTO eleitores (nome, cpf, predio, andar, numero_apt, senha) VALUES (?, ?, ?, ?, ?, ?)",
-      [nome, cpf, predio, andar, numero_apt, senha]
-    );
-    res.status(201).send(`Eleitor created with ID: ${result.insertId}`);
+    const eleitor = await Eleitor.findById(req.params.id);
+    res.render("eleitor/edit", { eleitor });
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
 exports.update = async (req, res) => {
-  const id = req.params.id;
-  const { nome, cpf, predio, andar, numero_apt, senha } = req.body;
-
-  // Verificar se todos os campos necessários estão presentes
-  if (!nome || !cpf || !predio || !andar || !numero_apt || !senha) {
-    return res.status(400).send("All fields are required");
-  }
-
   try {
-    const [result] = await pool.query(
-      "UPDATE eleitores SET nome = ?, cpf = ?, predio = ?, andar = ?, numero_apt = ?, senha = ? WHERE id = ?",
-      [nome, cpf, predio, andar, numero_apt, senha, id]
-    );
-    if (result.affectedRows === 0) {
-      return res.status(404).send("Eleitor not found");
-    }
-    res.send("Eleitor updated successfully");
+    await Eleitor.update(req.params.id, req.body);
+    res.redirect("/eleitores");
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
 exports.delete = async (req, res) => {
-  const id = req.params.id;
   try {
-    const [result] = await pool.query("DELETE FROM eleitores WHERE id = ?", [
-      id,
-    ]);
-    if (result.affectedRows === 0) {
-      return res.status(404).send("Eleitor not found");
-    }
-    res.send("Eleitor deleted successfully");
+    await Eleitor.delete(req.params.id);
+    res.redirect("/eleitores");
   } catch (error) {
     res.status(500).send(error.message);
   }
